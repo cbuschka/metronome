f83960a4f3c8fc8b067fe107526242191de57814
conni <cbuschka@gmail.com>
2023-03-12 20:14:38 +0100

    Changed worker loaded message.

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -3,23 +3,23 @@ var interval=100;
 
 self.onmessage=function(e){
 	if (e.data=="start") {
-		console.log("starting");
+		console.log("[worker] starting");
 		timerID=setInterval(function(){postMessage("tick");},interval);
 	}
 	else if (e.data.interval) {
-		console.log("setting interval");
+		console.log("[worker] setting interval");
 		interval=e.data.interval;
-		console.log("interval="+interval);
+		console.log("[worker] interval="+interval);
 		if (timerID) {
 			clearInterval(timerID);
 			timerID=setInterval(function(){postMessage("tick");},interval);
 		}
 	}
 	else if (e.data=="stop") {
-		console.log("stopping");
+		console.log("[Worker] stopping");
 		clearInterval(timerID);
 		timerID=null;
 	}
 };
 
-postMessage('hi there');
+postMessage('loaded');
